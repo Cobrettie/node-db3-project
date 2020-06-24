7f8dadf8cd4b41af034a9c281de5deeaec448302
Cobrettie <cobrettiegarner3@gmail.com>
2020-06-24 18:49:43 -0400

    configure dbConfigure.js

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,1 +1,18 @@
-const Schemes = require('')
+// const Schemes = require('')
+
+// find()
+
+
+// findById(id)
+
+
+// findSteps(id)
+
+
+// add(scheme)
+
+
+// update(changes, id)
+
+
+// remove(id)
